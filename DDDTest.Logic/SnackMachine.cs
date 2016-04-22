01268ca97b46f30eb868bac2d88d4a7ae4bc026d
tugrul <tugrulelmas@gmail.com>
2016-04-22 15:01:05 +0300

    first check-in

---
+++ DDDTest.Logic/SnackMachine.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using static DDDTest.Logic.Money;
+
+namespace DDDTest.Logic
+{
+    public sealed class SnackMachine : Entity
+    {
+        public Money MoneyInside { get; private set; } = None;
+        public Money MoneyInTransaction { get; private set; } = None;
+
+        public void InsertMoney(Money money) {
+            Money[] coinsAndNotes =
+            {
+                Cent, TenCent, Quarter, Dollar, FiveDollar, TwentyDollar
+            };
+            if (!coinsAndNotes.Contains(money))
+                throw new InvalidOperationException();
+
+            MoneyInTransaction += money;
+        }
+
+        public void ReturnMoney() {
+            MoneyInTransaction = None;
+        }
+
+        public void BuySnack() {
+            MoneyInside += MoneyInTransaction;
+            MoneyInTransaction = None;
+        }
+    }
+}
---
+++ DDDTest.UnitTests/MoneySpecs.cs
@@ -0,0 +1,26 @@
+﻿using DDDTest.Logic;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace DDDTest.UnitTests
+{
+    public class MoneySpecs
+    {
+        [Test]
+        public void Sum_of_two_moneys_produces_correct_result() {
+            Money money1 = new Money(1, 2, 3, 4, 5, 6);
+            Money money2 = new Money(1, 2, 3, 4, 5, 6);
+
+            var sum = money1 + money2;
+            sum.OneCentCount.Should().Be(2);
+        }
+
+        [Test]
+        public void Are_two_money_equal() {
+            Money money1 = new Money(1, 2, 3, 4, 5, 6);
+            Money money2 = new Money(1, 2, 3, 4, 5, 6);
+
+            money1.Should().Be(money2);
+        }
+    }
+}
